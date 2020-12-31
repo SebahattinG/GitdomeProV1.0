1c2d0eb4bb8c696e5d832dccdb1e3e068f534e03
SebahattinG <sebahattin.gokaydin@icloud.com>
2020-12-31 12:55:34 -0600

    Test 2 added

--- src/Default/Test2.java
+++ src/Default/Test2.java
@@ -3,7 +3,7 @@ package Default;
 public class Test2 {
 	public static void main(String [] args) {
 		
-		String hungry = "no";
+		String hungry = "yes";
 		
 		if (hungry == "yes")
 			System.out.println("eat something    ");
